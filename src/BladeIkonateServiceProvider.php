a9aefafbdac41182722456964988f56a7db55fe7
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2021-07-09 15:27:52 +0000

    Fix styling

--- src/BladeIkonateServiceProvider.php
+++ src/BladeIkonateServiceProvider.php
@@ -19,7 +19,6 @@ final class BladeIkonateServiceProvider extends ServiceProvider
 
             $factory->add('ikonate', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
-
     }
 
     private function registerConfig(): void
@@ -39,5 +38,4 @@ final class BladeIkonateServiceProvider extends ServiceProvider
             ], 'blade-ikonate-config');
         }
     }
-
 }
